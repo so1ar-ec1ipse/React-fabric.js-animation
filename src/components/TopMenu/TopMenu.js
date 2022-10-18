b360cf804dcddcc1c54b4b8c5b2b9d5c015aff60
ec1ipse <ec1ipse@so1ar.com>
2022-10-18 17:02:25 -0500

    private2public

--- src/components/TopMenu/TopMenu.js
+++ src/components/TopMenu/TopMenu.js
@@ -8,12 +8,6 @@ const TopMenu = () => {
     <div className='top-menu border-bottom border-dark'>
       <File />
       <span className='menu-item'>Edit</span>
-      <a
-        href='https://github.com/qvantor/canvas-2d-player'
-        target='_blank'
-        className='float-right text-white menu-item'>
-        <Icon type='github' /> Fork me on Github
-      </a>
     </div>
   )
 }
